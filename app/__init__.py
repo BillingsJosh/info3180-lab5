6987394cc1729a76982510550a543e16bbb5b42d
BillingsJosh <billingsjoshua07@gmail.com>
2025-04-13 17:29:24 -0500

    Exercise 3 completed

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,28 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
+
 from .config import Config
+from .models import Movie  # Optional, if needed elsewhere
 
-app = Flask(__name__)
-app.config.from_object(Config)
+from .models import db
 
-from app import views
+migrate = Migrate()
+csrf = CSRFProtect()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+    db.init_app(app)
+    migrate.init_app(app, db)
+    csrf.init_app(app)
+    
+    from .views import main
+    app.register_blueprint(main)
+
+    from app import views
+    return app
+
--- app/views.py
+++ app/views.py
@@ -1,63 +1,80 @@
-"""
-Flask Documentation:     https://flask.palletsprojects.com/
-Jinja2 Documentation:    https://jinja.palletsprojects.com/
-Werkzeug Documentation:  https://werkzeug.palletsprojects.com/
-This file creates your application.
-"""
-
-from app import app
-from flask import render_template, request, jsonify, send_file
+from flask import Blueprint, render_template, request, jsonify, send_file, current_app
+from werkzeug.utils import secure_filename
 import os
 
+from app.forms import MovieForm
+from app.models import Movie, db
 
-###
-# Routing for your application.
-###
+main = Blueprint('main', __name__)
 
-@app.route('/')
+# Home route
+@main.route('/')
 def index():
     return jsonify(message="This is the beginning of our API")
 
 
-###
-# The functions below should be applicable to all Flask apps.
-###
+# Route to handle movie submission (Exercise 3)
+@main.route('/api/v1/movies', methods=['POST'])
+def movies():
+    form = MovieForm()
+    
+    if form.validate_on_submit():
+        title = form.title.data
+        description = form.description.data
+        poster = form.poster.data
 
-# Here we define a function to collect form errors from Flask-WTF
-# which we can later use
-def form_errors(form):
-    error_messages = []
-    """Collects form errors"""
-    for field, errors in form.errors.items():
-        for error in errors:
-            message = u"Error in the %s field - %s" % (
-                    getattr(form, field).label.text,
-                    error
-                )
-            error_messages.append(message)
+        # Save the poster file securely
+        filename = secure_filename(poster.filename)
+        poster.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
 
-    return error_messages
+        # Save to database
+        new_movie = Movie(
+            title=title,
+            description=description,
+            poster=filename
+        )
+        db.session.add(new_movie)
+        db.session.commit()
 
-@app.route('/<file_name>.txt')
+        return jsonify({
+            "message": "Movie Successfully added",
+            "title": title,
+            "poster": filename,
+            "description": description
+        }), 201
+
+    return jsonify({"errors": form_errors(form)}), 400
+
+
+# Route to serve static .txt files (optional)
+@main.route('/<file_name>.txt')
 def send_text_file(file_name):
-    """Send your static text file."""
     file_dot_text = file_name + '.txt'
-    return app.send_static_file(file_dot_text)
+    return current_app.send_static_file(file_dot_text)
 
 
-@app.after_request
+# Disable browser caching (for dev convenience)
+@main.after_app_request
 def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also tell the browser not to cache the rendered page. If we wanted
-    to we could change max-age to 600 seconds which would be 10 minutes.
-    """
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
     response.headers['Cache-Control'] = 'public, max-age=0'
     return response
 
 
-@app.errorhandler(404)
+# Custom 404 error page
+@main.app_errorhandler(404)
 def page_not_found(error):
-    """Custom 404 page."""
     return render_template('404.html'), 404
+
+
+# Helper: Collect form validation errors
+def form_errors(form):
+    error_messages = []
+    for field, errors in form.errors.items():
+        for error in errors:
+            message = u"Error in the %s field - %s" % (
+                getattr(form, field).label.text,
+                error
+            )
+            error_messages.append(message)
+    return error_messages
