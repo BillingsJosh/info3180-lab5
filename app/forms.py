82305dc6fd73322202cbe4f3bf475dacd0d76adc
BillingsJosh <billingsjoshua07@gmail.com>
2025-04-13 15:12:48 -0500

    Added MovieForm class with title, description, and poster fields

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,9 @@
-# Add any form classes for Flask-WTF here
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, FileField
+from wtforms.validators import DataRequired
+from flask_wtf.file import FileAllowed
+
+class MovieForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    description = TextAreaField('Description', validators=[DataRequired()])
+    poster = FileField('Poster', validators=[FileAllowed(['jpg', 'jpeg', 'png'], 'Images only!')])
